861837ab7b1c49344fc4522242b5d1a8e96e3b54
Fred Munro <fredmunro7@gmail.com>
2021-04-05 11:03:28 -0400

    Completed responsiveness of landing page

--- Fred/scripts/index.js
+++ Fred/scripts/index.js
@@ -1,6 +1,7 @@
 
 window.onload = function() {
 
+    // Opens up the aisle header menu.
     let aisleList = document.getElementById("aisles"),
         aisleButton = document.getElementById("headerAisles");
     aisleButton.addEventListener("click", function() {
@@ -9,5 +10,12 @@ window.onload = function() {
         } else {
             aisleList.style.visibility = "hidden";
         }
+    })
+
+    // Allows to use the seachbar using enter.
+    let searchBar = document.getElementById("searchBar");
+    searchBar.addEventListener("keypress", function(e) {
+        if (e.key === 'Enter')
+            searchBar.value; // Value of searchbar upon pressing enter <--
     })
 }
