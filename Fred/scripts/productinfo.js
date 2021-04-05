a77cfbcc8d054648cb83814f9b3de3a68a582c74
Fred Munro <fredmunro7@gmail.com>
2021-04-05 14:35:54 -0400

    Reformatted Fred's file + deleted Shiloh's file

--- Fred/scripts/productinfo.js
+++ Fred/scripts/productinfo.js
@@ -1,62 +1,62 @@
 const dairy = [
     {
-        image: "images/milk1.jpg",
+        image: "images/dairy/milk1.jpg",
         company: "Natrel",
         name: "1% Fine Filtered Milk",
         price: "$5.19",
         description: "1% finely filtered milk for a pure taste."
     },
     {
-        image: "images/milk2.jpg",
+        image: "images/dairy/milk2.jpg",
         company: "Natrel",
         name: "2% Fine Filtered Milk",
         price: "$5.19",
         description: "2% finely filtered milk for a pure taste."
     },
     {
-        image: "images/milk3.jpg",
+        image: "images/dairy/milk3.jpg",
         company: "Natrel",
         name: "3.25% Fine Filtered Milk",
         price: "$5.19",
         description: "3.25% finely filtered milk for a pure taste."
     },
     {
-        image: "images/yogurt.jpg",
+        image: "images/dairy/yogurt.jpg",
         company: "Activia",
         name: "Stirred Vanilla Yogurt",
         price: "$4.29",
         description: "Vanilla yogurt, stirred for a creamier texture."
     },
     {
-        image: "images/yogurt-pack.jpg",
+        image: "images/dairy/yogurt-pack.jpg",
         company: "Yoplait",
         name: "Creamy Duo Assorted Yogurt",
         price: "$5.99",
         description: "Extra creamy yogurt containing two different flavors."
     },
     {
-        image: "images/kraft-singles.jpg",
+        image: "images/dairy/kraft-singles.jpg",
         company: "Kraft",
         name: "Singles Regular 22 Pack",
         price: "$5.79",
         description: "Individually packaged slices of cheddar cheese."
     },
     {
-        image: "images/cream-cheese.jpg",
+        image: "images/dairy/cream-cheese.jpg",
         company: "Philadelphia",
         name: "Soft Original Cream Cheese",
         price: "$5.99",
         description: "Original cream cheese without artificial flavors or colors."
     },
     {
-        image: "images/grated-cheese.jpg",
+        image: "images/dairy/grated-cheese.jpg",
         company: "Saputo",
         name: "Grated Quattro Fromaggi",
         price: "$18.99",
         description: "Contains four types of cheese grated and mixed together."
     },
     {
-        image: "images/whipped-cream.jpg",
+        image: "images/dairy/whipped-cream.jpg",
         company: "Québon",
         name: "20% Aerosol Whipped Cream",
         price: "$4.79",
@@ -66,67 +66,199 @@ const dairy = [
 
 const frozen = [
     {
-        image: "images/raspberry.jpg",
+        image: "images/frozen/frozen-strawberries.jpg",
         company: "Compliments",
-        name: "Whole Raspberries Frozen Fruit",
+        name: "Whole Strawberries Frozen Fruit",
         price: "$4.79",
-        description: "Frozen whole raspberries in a bag."
+        description: "Frozen whole strawberries in a bag."
     },
     {
-        image: "images/pizza.jpg",
+        image: "images/frozen/frozen-deluxe-pizza.jpg",
         company: "Mikes",
         name: "All-Dressed Frozen Pizza",
         price: "$7.99",
         description: "Oven baked frozen all-dressed pizza."
     },
     {
-        image: "images/juice.jpg",
+        image: "images/frozen/frozen-juice.jpg",
         company: "Minute Maid",
         name: "Fruit Punch Frozen Beverages",
         price: "$1.09",
         description: "Frozen concentrated fruit punch juice base."
     },
     {
-        image: "images/pasta.jpg",
+        image: "images/frozen/frozen-pasta.jpg",
         company: "Michelina's",
         name: "Penne Pollo Frozen Entree",
         price: "$2.69",
         description: "Penne pollo microwaveable frozen dinner"
     },
     {
-        image: "images/turkey.jpg",
+        image: "images/frozen/frozen-turkey-dinner.jpg",
         company: "Swanson",
         name: "Turkey Frozen Dinner",
         price: "$4.19",
         description: "Turkey microwaveable frozen dinner."
     },
     {
-        image: "images/steak.jpg",
+        image: "images/frozen/frozen-steak-dinner.jpg",
         company: "Hungry Man",
         name: "Salisbury Steak Frozen Entree",
         price: "$5.29",
         description: "Steak microwaveable frozen dinner."
     },
     {
-        image: "images/minipizza.jpg",
+        image: "images/frozen/frozen-mini-pizzas.jpg",
         company: "Plats Du Chef",
         name: "Pepperoni Frozen Pizzettas",
         price: "$3.79",
         description: "Oven baked frozen pepperoni pizzettas."
     },
     {
-        image: "images/broccoli.jpg",
-        company: "Green Organic",
-        name: "Broccoli Frozen Vegetables",
-        price: "$4.99",
-        description: "Frozen broccoli flowers in a bag."
+        image: "images/frozen/frozen-fries.jpg",
+        company: "McCain",
+        name: "Straight Cut SuperFries",
+        price: "$3.99",
+        description: "Frozen straight cut superfries."
     },
     {
-        image: "images/icecream.jpg",
+        image: "images/frozen/vanilla-icecream.jpg",
         company: "Nestle",
         name: "Parlour Vanilla Frozen Dessert",
         price: "$3.99",
         description: "Tub of vanilla ize cream."
+    }
+];
+
+const meats = [
+    {
+        image: "images/meat/medium-ground-beef.jpg",
+        company: "100g",
+        name: "Medium Ground Beef",
+        price: "$1.50",
+        description: "Freshly cut medium ground beef."
+    },
+    {
+        image: "images/meat/lean-ground-beef.jpg",
+        company: "100g",
+        name: "Lean Ground Beef",
+        price: "$1.72",
+        description: "Freshly cut lean ground beef."
+    },
+    {
+        image: "images/meat/extra-lean-ground-beef.jpg",
+        company: "100g",
+        name: "Extra Lean Ground Beef",
+        price: "$1.87",
+        description: "Freshly cut extra lean ground beef."
+    },
+    {
+        image: "images/meat/smoked-meat.jpg",
+        company: "100g",
+        name: "Smoked Meat",
+        price: "$3.99",
+        description: "Freshly sliced smoked meat."
+    },
+    {
+        image: "images/meat/wine-meat.jpg",
+        company: "100g",
+        name: "Red wine Beef",
+        price: "$4.85",
+        description: "Freshly cut red wine beef."
+    },
+    {
+        image: "images/meat/tourtiere.jpg",
+        company: "Plaisirs Gastronomiques",
+        name: "Meat Pie",
+        price: "$12.29",
+        description: "Packaged meat pie."
+    },
+    {
+        image: "images/meat/maple-beef.jpg",
+        company: "170g",
+        name: "Maple Ground Beef Patties",
+        price: "$3.74",
+        description: "Fershly cut maple ground beef patties."
+    },
+    {
+        image: "images/meat/pork-beef-sausage.jpg",
+        company: "Lafleur",
+        name: "Pork Beef Sausage",
+        price: "$5.49",
+        description: "Packaged pork beef sausages."
+    },
+    {
+        image: "images/meat/salami-deli-meat.jpg",
+        company: "Marcangelo",
+        name: "Salami Deli Meat",
+        price: "$6.79",
+        description: "Sliced deli salami."
+    }
+];
+
+const produce = [
+    {
+        image: "images/produce/smith-apples.jpg",
+        company: "180g",
+        name: "Granny Smith Apples",
+        price: "$1.19",
+        description: "Fresh granny smith apples."
+    },
+    {
+        image: "images/produce/honeycrisp-apples.jpg",
+        company: "180g",
+        name: "Honeycrisp Apples",
+        price: "$1.58",
+        description: "Fresh honeycrisp apples."
+    },
+    {
+        image: "images/produce/bananas.jpg",
+        company: "210g",
+        name: "Bananas",
+        price: "$0.37",
+        description: "Fresh bananas."
+    },
+    {
+        image: "images/produce/blueberries.jpg",
+        company: "170g",
+        name: "Blueberries",
+        price: "$4.99",
+        description: "Fresh blueberries in a clam shell."
+    },
+    {
+        image: "images/produce/celery.jpg",
+        company: "450g",
+        name: "Celery",
+        price: "$3.29",
+        description: "Fresh celery."
+    },
+    {
+        image: "images/produce/green-lettuce.jpg",
+        company: "360g",
+        name: "Green Leaf Lettuce",
+        price: "$2.49",
+        description: "Fresh green leaf lettuce."
+    },
+    {
+        image: "images/produce/romaine-lettuce.jpg",
+        company: "360g",
+        name: "Romaine Lettuce",
+        price: "$4.99",
+        description: "Fresh romaine lettuce."
+    },
+    {
+        image: "images/produce/broccoli.jpg",
+        company: "250g",
+        name: "Broccoli",
+        price: "$3.99",
+        description: "Fresh broccoli flowers."
+    },
+    {
+        image: "images/produce/strawberries.jpg",
+        company: "454g",
+        name: "Strawberries",
+        price: "6.99",
+        description: "Fresh strawberries in a clam shell."
     }
 ];
 
@@ -140,6 +272,10 @@ function makeTable(aisle) {
         products = dairy;
     } else if (aisle === 'frozen') {
         products = frozen;
+    } else if (aisle === 'meats') {
+        products = meats;
+    } else if (aisle === 'produce') {
+        products = produce;
     }
 
     let rowCounter = 0,
